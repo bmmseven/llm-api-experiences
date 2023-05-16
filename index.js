e0acbd181b3e92b35b3328da4080619891e5fb96
Bruno Silva <bm222gf@student.lnu.se>
2023-05-16 23:01:53 +0200

    Added user prompts, adapted the system prompts to the API commands we are going to use

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const { Configuration, OpenAIApi } = require("openai");
 
+// Include list of API commands
+
+// Loop through list of promts
+// For each prompt, inject the list of API commands
+// Loop through list of user_prompts, each line is a new user prompt
+// Save the output to a file, if the experience need additional input from the user, ask it before saving it
+// Repeat until all prompts are done
+
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
